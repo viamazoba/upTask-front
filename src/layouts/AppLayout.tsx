713510b7891848cfdd7eff5645d2ee6fd21338ee
Victor Mazo <viamazoba@unal.edu.co>
2024-09-16 22:47:34 -0500

    feat: Se genera enrutamiento hacia CreateProjectView.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,7 @@
 
 import { Outlet } from 'react-router-dom'
 import Logo from '@/components/Logo'
+import NavMenu from '@/components/NavMenu'
 
 export default function AppLayout() {
 
@@ -14,7 +15,7 @@ export default function AppLayout() {
                     <div className='w-64'>
                         <Logo />
                     </div>
-
+                    <NavMenu />
                 </div>
             </header>
             <section className='max-w-screen-2xl mx-auto mt-10 p-5'>
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from '@/layouts/AppLayout'
 import DashboardView from '@/views/DashboardView'
+import CreateProjectView from './views/projects/CreateProjectView'
 
 export default function Router() {
 
@@ -9,6 +10,7 @@ export default function Router() {
             <Routes>
                 <Route element={<AppLayout/>}>
                     <Route path='/'  element={<DashboardView />} index/>
+                    <Route path='/projects/create'  element={<CreateProjectView />} />
                 </Route>
             </Routes>
         </BrowserRouter>
