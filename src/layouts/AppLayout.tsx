a3b0feb65616ee516735e673f91baae044fa4564
Victor Mazo <viamazoba@unal.edu.co>
2024-09-16 22:30:56 -0500

    feat: Se mejora enrutamiento de las carpetas.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from 'react-router-dom'
-import Logo from '../components/Logo'
+import Logo from '@/components/Logo'
 
 export default function AppLayout() {
 
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
-import DashboardView from './views/DashboardView'
+import AppLayout from '@/layouts/AppLayout'
+import DashboardView from '@/views/DashboardView'
 
 export default function Router() {
 
