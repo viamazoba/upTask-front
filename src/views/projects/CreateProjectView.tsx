68d44044a7a1fa5f0b4ee972fb8f0f9ee3ca86bd
Victor Mazo <viamazoba@unal.edu.co>
2024-09-20 19:38:47 -0500

    feat: Se re-direcciona la persona al crear proyecto.

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from 'react-hook-form'
 import ProjectForm from "@/components/projects/ProjectFrom";
 import { ProjectFormData } from "@/types/index";
-import createProject from "@/api/ProjectAPI";
+import { createProject } from "@/api/ProjectAPI";
 
 
 export default function CreateProjectView() {
 
+    const navigate = useNavigate()
     const initialValues:ProjectFormData = {
         projectName: '',
         clientName: '',
@@ -17,8 +18,9 @@ export default function CreateProjectView() {
         defaultValues: {initialValues}
     })
 
-    const handleForm = (data: ProjectFormData) => {
-        createProject(data)
+    const handleForm = async (data: ProjectFormData) => {
+        await createProject(data)
+        navigate('/')
     }
 
     return(
