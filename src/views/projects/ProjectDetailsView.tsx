a6934bde5f283e28b55087159417c8b808f6a375
Victor Mazo <viamazoba@unal.edu.co>
2024-09-23 22:00:02 -0500

    feat: Se agrega vista de detalle de cada proyecto y un modal como reacción al añadir un proyecto.

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,9 +1,43 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getProjectById } from "@/api/ProjectAPI"
+import AddTaskModal from "@/components/tasks/AddTaskModal"
 
-export default function ProductDetailsView() {
 
-    return (
-        <div>
-            ProjectDetailsView
-        </div>
+export default function ProjectDetailsView() {
+
+    const navigate = useNavigate()
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['editProject', projectId],
+        queryFn: () => getProjectById(projectId),
+        retry: false
+    })
+
+    console.log(isLoading)
+    console.log(data)
+
+    if (isLoading) return 'Cargando ...'
+    if (isError) return <Navigate to='/404' />
+    if (data) return (
+        <>
+            <h1 className="text-5xl font-black">{data.projectName}</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+            <nav className="my-5 flex gap-3">
+                <button
+                    type="button"
+                    className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                    onClick={() => navigate(location.pathname + '?newTask=true')}
+                >
+                    Agregar Tarea
+                </button>
+            </nav>
+
+            <AddTaskModal />
+        </>
     )
+
 }
