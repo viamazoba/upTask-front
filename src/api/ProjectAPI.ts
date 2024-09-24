1a80ace10f143de600705eb576330f141911e573
Victor Mazo <viamazoba@unal.edu.co>
2024-09-23 21:18:54 -0500

    feat: Se implementa lacción de eliminar proyectos.

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -60,3 +60,15 @@ export async function updateProject({formData, projectId}: ProjectAPIType) {
         }
     }
 }
+
+export async function deleteProject(id: Project['_id']) {
+    try {
+        const { data } = await api.delete<string>(`/projects/${id}`)
+        return data
+
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -2,13 +2,26 @@ import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import { getProjects } from '@/api/ProjectAPI'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { deleteProject, getProjects } from '@/api/ProjectAPI'
+import { toast } from 'react-toastify'
 
 export default function DashboardView() {
     const { data, isError, isLoading } = useQuery({
         queryKey: ['projects'],
         queryFn: getProjects
+    })
+
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: deleteProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({ queryKey: ['projects'] })
+        }
     })
 
     if (data) return (
@@ -72,7 +85,7 @@ export default function DashboardView() {
                                                 <button
                                                     type='button'
                                                     className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                                    onClick={() => { }}
+                                                    onClick={() => mutate(project._id)}
                                                 >
                                                     Eliminar Proyecto
                                                 </button>
