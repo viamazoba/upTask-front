6049890de9924ce747547461b2491e978f909be0
Victor Mazo <viamazoba@unal.edu.co>
2024-09-24 22:27:36 -0500

    feat: Se finaliza maquetado de la visual de las tareas.

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,6 +17,22 @@ const initialStatusGroups: GroupTasks = {
     completed: []
 }
 
+const statusStyles: { [key: string]: string } = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-emerald-500'
+}
+
+const statusTranslations: { [key: string]: string } = {
+    pending: 'Pendiente',
+    onHold: 'En Espera',
+    inProgress: 'En Progreso',
+    underReview: 'En Revisión',
+    completed: 'Completado'
+}
+
 export default function TaskList({
     tasks
 }: TaskListProps) {
@@ -34,6 +50,12 @@ export default function TaskList({
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+
+                        <h3
+                            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                        >
+                            {statusTranslations[status]}
+                        </h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
