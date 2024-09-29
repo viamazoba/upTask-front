2d9dbcd9446989609534cc113853edefb07cdb36
Victor Mazo <viamazoba@unal.edu.co>
2024-09-29 18:46:54 -0500

    feat: Se avanza en la implementación de detalle de la tarea de un projecto.

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -59,7 +59,11 @@ export default function TaskCard({
                         <Menu.Items
                             className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                             <Menu.Item>
-                                <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                                <button
+                                    type='button'
+                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                    onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
+                                >
                                     Ver Tarea
                                 </button>
                             </Menu.Item>
---
+++ src/components/tasks/TaskModalDetails.tsx
@@ -0,0 +1,79 @@
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getTaskById } from '@/api/TaskAPI';
+import { toast } from 'react-toastify';
+
+
+export default function TaskModalDetails() {
+
+    const params = useParams()
+    const projectId = params.projectId!
+    const navigate = useNavigate()
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const taskId = queryParams.get('viewTask')!
+
+    const show = taskId ? true : false
+
+    const { data, isError, error } = useQuery({
+        queryKey: ['task', taskId],
+        queryFn: () => getTaskById({ projectId, taskId }),
+        enabled: !!taskId,
+        retry: false
+    })
+
+    if (isError) {
+        toast.error(error.message, { toastId: 'error' })
+        return <Navigate to={`/projects/${projectId}`} />
+    }
+
+    return (
+        <>
+            <Transition appear show={show} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname, { replace: true })}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-black/60" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                                    <p className='text-sm text-slate-400'>Agregada el: </p>
+                                    <p className='text-sm text-slate-400'>Última actualización: </p>
+                                    <Dialog.Title
+                                        as="h3"
+                                        className="font-black text-4xl text-slate-600 my-5"
+                                    >Titulo aquí
+                                    </Dialog.Title>
+                                    <p className='text-lg text-slate-500 mb-2'>Descripción:</p>
+                                    <div className='my-5 space-y-3'>
+                                        <label className='font-bold'>Estado Actual:</label>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    )
+}
