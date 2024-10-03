47204c1405428dfc07f0521058e596a440ad658e
Victor Mazo <viamazoba@unal.edu.co>
2024-10-02 21:31:17 -0500

    feat: Se implementa el cambio de estado de las tareas.

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 /** Tasks */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
+export type TaskStatus = z.infer<typeof taskStatusSchema>
+
 export const taskSchema = z.object({
     _id: z.string(),
     name: z.string(),
