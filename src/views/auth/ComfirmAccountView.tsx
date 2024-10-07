a7280c9bbb0bcac496b09534257041c2528999e0
Victor Mazo <viamazoba@unal.edu.co>
2024-10-06 20:26:53 -0500

    feat: Se implementa la validación del token con el backend.

--- src/views/auth/ComfirmAccountView.tsx
+++ src/views/auth/ComfirmAccountView.tsx
@@ -2,18 +2,29 @@ import { Link } from "react-router-dom";
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
 import { useState } from "react";
 import { ConfirmationToken } from "@/types/index";
+import { confirmAccount } from "@/api/AuthAPI";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 export default function ConfirmAccountView() {
 
     const [token, setToken] = useState<ConfirmationToken['token']>('')
 
+    const { mutate } = useMutation({
+        mutationFn: confirmAccount,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
     const handleChange = (token: ConfirmationToken['token']) => {
         setToken(token)
     }
 
-    const handleComplete = (token: ConfirmationToken['token']) => {
-        console.log(token)
-    }
+    const handleComplete = (token: ConfirmationToken['token']) => mutate({ token })
 
     return (
         <>
