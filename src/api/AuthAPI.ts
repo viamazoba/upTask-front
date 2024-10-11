be331ec990f3c531060bbe717e81c3949dae5188
Victor Mazo <viamazoba@unal.edu.co>
2024-10-10 21:21:53 -0500

    feat: Se finaliza vista para pedir nuevo código de confirmación.

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmationToken, UserRegistrationForm } from "../types";
+import { ConfirmationToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
     try {
@@ -18,6 +18,19 @@ export async function createAccount(formData: UserRegistrationForm) {
 export async function confirmAccount(formData: ConfirmationToken) {
     try {
         const url = '/auth/confirm-account'
+        const { data } = await api.post<string>(url, formData)
+        return data
+
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
+    try {
+        const url = '/auth/request-code'
         const { data } = await api.post<string>(url, formData)
         return data
 
