534495e37b99d39617ae6994d49473be05f80999
Victor Mazo <viamazoba@unal.edu.co>
2024-11-04 21:56:14 -0500

    feat: Se implementa jwt en el fronted y se guarda en el localStorage.

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -45,6 +45,8 @@ export async function authenticateUser(formData: UserLoginForm) {
     try {
         const url = '/auth/login'
         const { data } = await api.post<string>(url, formData)
+
+        localStorage.setItem('AUTH_TOKEN', data)
         return data
 
     } catch (error) {
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -19,8 +19,8 @@ export default function LoginView() {
         onError: (error) => {
             toast.error(error.message)
         },
-        onSuccess: (data) => {
-            toast.success(data)
+        onSuccess: () => {
+            toast.success('Iniciando Sesión')
         }
     })
 
