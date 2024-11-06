37b77c2270e597ce7aa9c891ae4e84c9765e30a6
Victor Mazo <viamazoba@unal.edu.co>
2024-11-05 22:08:37 -0500

    feat: Se implementa interceptor en axios para enviar token, y se borra el código dedicado ddel token en ProjectAPI.ts

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -3,13 +3,13 @@ import { dashboardProjectSchema, Project, ProjectFormData } from "@/types/index"
 import { isAxiosError } from "axios";
 
 
-export async function createProject(formData:ProjectFormData) {
+export async function createProject(formData: ProjectFormData) {
     try {
         const { data } = await api.post('/projects', formData)
         return data
 
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
@@ -17,16 +17,17 @@ export async function createProject(formData:ProjectFormData) {
 
 
 export async function getProjects() {
+
     try {
         const { data } = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
 
-        if(response.success) {
+        if (response.success) {
             return response.data
         }
 
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
@@ -38,7 +39,7 @@ export async function getProjectById(id: Project['_id']) {
         return data
 
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
@@ -49,13 +50,13 @@ type ProjectAPIType = {
     projectId: Project['_id']
 }
 
-export async function updateProject({formData, projectId}: ProjectAPIType) {
+export async function updateProject({ formData, projectId }: ProjectAPIType) {
     try {
         const { data } = await api.put<string>(`/projects/${projectId}`, formData)
         return data
 
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
@@ -67,7 +68,7 @@ export async function deleteProject(id: Project['_id']) {
         return data
 
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
