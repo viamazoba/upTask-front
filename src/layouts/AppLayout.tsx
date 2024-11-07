8508864c91f87e5505ea9e65ecdd4276d9aa3ac3
Victor Mazo <viamazoba@unal.edu.co>
2024-11-06 21:41:19 -0500

    feat: Se agrega el nombre del usuario al menu hamburguesa.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -27,7 +27,9 @@ export default function AppLayout() {
                             <Logo />
                         </Link>
                     </div>
-                    <NavMenu />
+                    <NavMenu
+                        name={data.name}
+                    />
                 </div>
             </header>
             <section className='max-w-screen-2xl mx-auto mt-10 p-5'>
