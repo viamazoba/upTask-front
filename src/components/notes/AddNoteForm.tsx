c0d66a0bbeaa89da2ba79d5267426120cf3449f1
Victor Mazo <viamazoba@unal.edu.co>
2024-11-18 22:18:22 -0500

    feat: Se integra componente para crear notas con el backend.

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,10 +1,46 @@
-
+import { NoteFormData } from "@/types/index"
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../ErrorMessage"
+import { useMutation } from "@tanstack/react-query"
+import { createNote } from "@/api/NoteAPI"
+import { toast } from "react-toastify"
+import { useLocation, useParams } from "react-router-dom"
 
 export const AddNoteForm = () => {
 
+    const params = useParams()
+    const location = useLocation()
+
+    const queryParams = new URLSearchParams(location.search)
+
+    const projectId = params.projectId!
+    const taskId = queryParams.get('viewTask')!
+    const initialValues: NoteFormData = {
+        content: ''
+    }
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        defaultValues: initialValues
+    })
+
+    const { mutate } = useMutation({
+        mutationFn: createNote,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
+    const handleAddNote = (formData: NoteFormData) => {
+        mutate({ projectId, taskId, formData })
+        reset()
+    }
+
     return (
         <form
-            onSubmit={() => { }}
+            onSubmit={handleSubmit(handleAddNote)}
             className="space-y-3"
             noValidate
         >
@@ -22,8 +58,17 @@ export const AddNoteForm = () => {
                     type="text"
                     placeholder="Contenido de la nota"
                     className="w-full p-3 border border-gray-300"
+                    {
+                    ...register('content', {
+                        required: 'El contenido de la nota es obligatorio'
+                    })
+                    }
                 />
-
+                {
+                    errors.content && (
+                        <ErrorMessage>{errors.content.message}</ErrorMessage>
+                    )
+                }
             </div>
 
             <input
