0959056016c86a544cbb3dfb216099f4bcf70262
Victor Mazo <viamazoba@unal.edu.co>
2024-11-19 22:42:32 -0500

    feat: Se implementa opción de eliminar nota por la persona que la ha creado.

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,11 +1,40 @@
+import { deleteNote } from "@/api/NoteAPI"
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type NoteDetailProps = {
     note: Note
 }
 
 export const NoteDetail = ({ note }: NoteDetailProps) => {
+
+    const { data, isLoading } = useAuth()
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+    const params = useParams()
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+
+    const projectId = params.projectId!
+    const taskId = queryParams.get('viewTask')!
+
+    const queryClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: deleteNote,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({ queryKey: ['task', taskId] })
+        }
+    })
+
+    if (isLoading) return 'Cargando ...'
 
     return (
         <div className="p-3 flex justify-between items-center">
@@ -17,6 +46,19 @@ export const NoteDetail = ({ note }: NoteDetailProps) => {
                     {formatDate(note.createdAt)}
                 </p>
             </div>
+
+            {
+                canDelete && (
+
+                    <button
+                        type="button"
+                        className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+                        onClick={() => mutate({ projectId, taskId, noteId: note._id })}
+                    >
+                        Eliminar
+                    </button>
+                )
+            }
         </div>
     )
 }
